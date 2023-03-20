9b105a1e5fcb884e0a6014cfbf0205ba9f8375e4
mmaj3047 <72222901+mmaj3047@users.noreply.github.com>
2023-03-19 22:52:30 -0400

    Added code for proper logging

--- fitness.js
+++ fitness.js
@@ -27,6 +27,19 @@ try {
   console.log("Could not connect to the database");
 }
 
+app.use((req, res, next) => {
+  console.log(
+    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  );
+  console.log("Got Request!");
+  req.method ? console.log("Method:", req.method) : null;
+  req.originalUrl ? console.log("Original URL:", req.originalUrl) : null;
+  req.get("Authorization")
+    ? console.log("Authorization:", req.get("Authorization"))
+    : null;
+  next();
+});
+
 app.use("/food", foodRouter);
 app.use("/recipes", recipeRouter);
 app.use("/exercise/cardio", cardioRouter);
